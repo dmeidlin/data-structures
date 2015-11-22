091e89376a0c3cae11faa63e21ddf635d967c717
dmeidlin <git_hub@nym.hush.com>
2015-11-22 15:27:55 -0500

    Make queue and stack in a functional-shared class pattern.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,45 @@
-var Queue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-};
+/*jslint browser: true, devel: true, windows: true,
+forin: true, vars: true, nomen: true, plusplus: true, bitwise: true,
+regexp: true, indent: 2, maxerr: 50 */
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  'use strict';
+  this.storage[this.storage.back++] = value;
+};
+
+queueMethods.dequeue = function () {
+  'use strict';
+  var firstValueIn = this.storage[this.storage.front++];
+
+  delete this.storage[this.storage.front - 1];
+  return firstValueIn;
+};
+
+queueMethods.size = function () {
+  'use strict';
+  return Object.keys(this.storage).length - 2;
+};
+
+
+var Queue = function () {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+  'use strict';
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  someInstance.storage = {};
+  someInstance.storage.back = 1;
+  someInstance.storage.front = 1;
+
+  // Implement the methods below
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
+  someInstance.size = queueMethods.size;
+
+  return someInstance;
+
+};
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,44 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+/*jslint browser: true, devel: true, windows: true,
+forin: true, vars: true, nomen: true, plusplus: true, bitwise: true,
+regexp: true, indent: 2, maxerr: 50 */
+
+//Create a global object to hold shared methods.
+var stackMethods = {};
+
+stackMethods.push = function (value) {
+  'use strict';
+  this.storage[Object.keys(this.storage).length + 1] = value;
 };
 
-var stackMethods = {};
+stackMethods.pop = function () {
+  'use strict';
+  var lastValueIn = this.storage[Object.keys(this.storage).length];
+  delete this.storage[Object.keys(this.storage).length];
+  return lastValueIn;
+};
+
+stackMethods.size = function () {
+  'use strict';
+  return Object.keys(this.storage).length;
+};
+
+//Create a constructor using the functional-shared pattern
+var Stack = function () {
+  'use strict';
+  var someInstance = {};
+
+  // Use an object with numeric keys to store values
+  someInstance.storage = {};
+
+  // Implement the methods below
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+
+  return someInstance;
+};
+
+
+
 
 
